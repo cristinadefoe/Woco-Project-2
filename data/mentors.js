70f3acc131a7be763231baf1b9115459b3f794b5
Cristina Defoe <cristina_defoe@yahoo.com>
2018-06-18 06:21:18 -0600

    Update files

--- data/mentors.js
+++ data/mentors.js
@@ -2,7 +2,7 @@ var mentorsArray = [
     {
         name: "Amy Adler",
         photo: "https://user-images.githubusercontent.com/35505038/40946064-e54cd9ca-6818-11e8-9d80-da115b3b3d23.jpg",
-        // email: "amy@woco.com",
+        email: "amy@woco.com",
         scores: [
             "1",
             "2",
@@ -19,7 +19,7 @@ var mentorsArray = [
     {
         name: "Desiree Bardot",
         photo: "https://user-images.githubusercontent.com/35505038/40946120-2685a1ba-6819-11e8-937a-610943c97580.jpg",
-        // email: "desiree@woco.com",
+        email: "desiree@woco.com",
         scores: [
             "2",
             "3",
@@ -36,7 +36,7 @@ var mentorsArray = [
     {
         name: "Ashley Dixon",
         photo: "https://user-images.githubusercontent.com/35505038/40946147-4936de2c-6819-11e8-9936-4a486631d81b.jpg",
-        // email: "ashley@woco.com",
+        email: "ashley@woco.com",
         scores: [
             "5",
             "2",
