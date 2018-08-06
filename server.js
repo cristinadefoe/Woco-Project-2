a6e9726f4e3d30d73ad4d2866fef8b8f58bf5a2e
Cristina Defoe <cristina_defoe@yahoo.com>
2018-08-05 21:03:11 -0600

    Update files

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var passport = require('./config/passport');
 
 // Port will work on local host 8080
 var PORT = process.env.PORT || 8080;
@@ -14,34 +13,20 @@ var db = require('./models');
 var app = express();
 
 // Standard bodyParser
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+// Parse application/json
 app.use(bodyParser.json());
-
-// // Check if server is running when press node.js
-// app.use(bodyParser.text({ type: "text/html" }));
-// app.use(bodyParser.json({ type: "application/vnd.custom-type" }));
 
 // Serve static files and assign to public folder so it's available to all documents
 app.use(express.static('public'));
-
-// Use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Routes
 require("./routes/dating-api-routes.js")(app);
 require("./routes/mentor-api-routes.js")(app);
 require("./routes/friend-api-routes.js")(app);
 
-// Require api-routes file to server (passport authentication)
-require("./routes/api-routes.js")(app);
-
+// Require api-routes to server
 require('./routes/main-html-routes.js')(app);
-
-// Require html-routes file to server (passport authentication)
-require('./routes/html-routes.js')(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function () {
